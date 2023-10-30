a39c8265823b8a52077c960a44f2fc4e0415a6a3
Justin <justinstolk97@gmail.com>
2023-10-30 12:28:02 +0100

    Made the statue quest work

--- Assets/Scripts/Monuments/TitanStatue.cs
+++ Assets/Scripts/Monuments/TitanStatue.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class TitanStatue : MonoBehaviour, IInteractable
@@ -9,6 +10,14 @@ public class TitanStatue : MonoBehaviour, IInteractable
     [SerializeField] private QuestCondition condition;
     [SerializeField] private PlayerInput input;
 
+    [SerializeField] private UnityEvent onQuestFinished;
+    [SerializeField] private UnityEvent onWrongPicturePresented;
+
+    [SerializeField] private Material debugSwapMaterial;
+
+    private bool questFinished;
+
+
     // Possible quests:
     //      Show a picture of a specific object.
     //      Show a picture of a specific behaviour.
@@ -16,6 +25,8 @@ public class TitanStatue : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+        if (questFinished) return;
+
         Scrapbook.Instance.OpenPages();
         ScrapbookPanel.Instance.OpenPanel();
         Cursor.lockState = CursorLockMode.Confined;
@@ -24,7 +35,7 @@ public class TitanStatue : MonoBehaviour, IInteractable
         foreach(PagePicture picture in Scrapbook.Instance.GetCollectedPictures())
         {
             picture.OnPictureClicked -= picture.SelectForPlacement;
-            picture.OnPictureClicked += () => { ShowPicture(picture.PictureInfo); };
+            picture.OnPictureClicked += () => { ShowPicture(picture); };
             //picture.OnPictureClicked = () => { ShowPicture(picture.PictureInfo); Debug.Log("Clicked on a picture that has to be shown now"); };
         }
 
@@ -33,17 +44,29 @@ public class TitanStatue : MonoBehaviour, IInteractable
         // Question: Does showing the picture consume it?
     }
 
-    public void ShowPicture(PictureInfo pictureInfo)
+    public void ShowPicture(PagePicture picture)
     {
         // To do: Evaluate whether any of the objects in the picture info is the object that we're looking for/
         // Also check if there are additional conditions and evaluate these too.
-        if (condition.Evaluate(pictureInfo))
+        if (condition.Evaluate(picture.PictureInfo))
         {
             Debug.Log("Something very cool happens");
+            onQuestFinished?.Invoke();
+            Destroy(picture.gameObject);
+            questFinished = true;
         }
         else
         {
+            onWrongPicturePresented?.Invoke();
             Debug.Log("That wasn't the droid, uh... answer I was looking for");
+        }
+    }
+
+    public void DebugChangeMaterialVisuals()
+    {
+        foreach(MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>())
+        {
+            renderer.material = debugSwapMaterial;
         }
     }
 }
