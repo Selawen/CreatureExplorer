889dd0e7eb17fc9d25ea82ef82183da5c8c0bfcb
Selawen <37039183+Selawen@users.noreply.github.com>
2023-11-30 16:00:49 +0100

    Reworked progresstracker to accept nested categories

--- Assets/Scripts/ProgressTracker/ProgressCategory.cs
+++ Assets/Scripts/ProgressTracker/ProgressCategory.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class ProgressCategory : Progress
+public class ProgressCategory
 {
-    [field: SerializeField] public Progress[] Progresses { get; private set; }
+    [field: SerializeField] public string Name { get; private set; }
+    [field: ShowOnly(2)] [field: SerializeField] public float Percentage { get; protected set; }
+    [field: Range(1, 100)] [field: SerializeField] public int TotalAmount { get; protected set; }
+    [field: SerializeField] public int PlayerAmount { get; protected set; }
+    [field: ShowOnly] [field: SerializeField] public bool Completed { get; protected set; }
+
+    private ProgressCategory category;
+
+    [field: SerializeField] public ProgressCategory[] Tracked { get; private set; }
 
     public void Initialise()
     {
-        TotalAmount = Progresses.Length;
+        if (Tracked.Length < 1)
+            return;
 
-        foreach (Progress p in Progresses)
+        TotalAmount = Tracked.Length;
+
+        foreach (ProgressCategory p in Tracked)
         {
             p.Initialise(this);
         }
@@ -19,30 +30,79 @@ public class ProgressCategory : Progress
         Update();
     }
 
+    public void Initialise(ProgressCategory parent)
+    {
+        category = parent;
+        if (Tracked.Length < 1)
+        {
+            PlayerAmount = Mathf.Clamp(PlayerAmount, 0, TotalAmount);
+            Percentage = (float)PlayerAmount / TotalAmount;
+
+            UpdateCompletion();
+        } else
+        {
+            Initialise();
+        }
+    }
+
+    public void SetName(string name)
+    {
+        Name = name;
+    }
+
+    public void AddProgress(int amount = 1)
+    {
+        PlayerAmount += 1;
+        PlayerAmount = Mathf.Clamp(PlayerAmount, 0, TotalAmount);
+        Percentage = Percentage = (float)PlayerAmount / TotalAmount;
+
+        UpdateCompletion();
+    }
+
+    public void SetProgress(int amount)
+    {
+        PlayerAmount = amount;
+        PlayerAmount = Mathf.Clamp(PlayerAmount, 0, TotalAmount);
+        Percentage = Percentage = (float)PlayerAmount / TotalAmount;
+
+        UpdateCompletion();
+    }
+
+    protected void UpdateCompletion()
+    {
+        if (Percentage >= 1)
+        {
+            Completed = true;
+        }
+
+        if (category != null)
+        {
+            category.Update();
+        }
+
+    }
+
     public void Update()
     {
-        if (completed)
+        if (Completed || Tracked.Length<1)
             return;
 
         float averageProgress = 0;
         int completedCount = 0;
 
-        foreach (Progress p in Progresses)
+        foreach (ProgressCategory p in Tracked)
         {
             averageProgress += p.Percentage;
 
-            completedCount += p.completed ? 1 : 0;
+            completedCount += p.Completed ? 1 : 0;
         }
 
-        averageProgress /= Progresses.Length;
+        averageProgress /= Tracked.Length;
 
         Percentage = averageProgress;
 
         SetProgress(completedCount);
 
-        if (Percentage >= 1)
-        {
-            completed = true;
-        }
+        UpdateCompletion();
     }
 }
