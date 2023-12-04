65ab52bb3e2275ee736b97fae64980e9e963e7af
Justin <justinstolk97@gmail.com>
2023-12-04 16:00:55 +0100

    Temple quest unlocks and berry throw
    Can now throw berries. Their graphics have also been updated.
    Broken bushes fixed.

--- Assets/Scripts/Reactivity/ItemEffects/Throwable.cs
+++ Assets/Scripts/Reactivity/ItemEffects/Throwable.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
 
-public class Throwable : StatusEffect
+[RequireComponent(typeof(Rigidbody))]
+public class Throwable : StatusEffect, IInteractable
 {
+    [field: SerializeField] public string InteractionPrompt { get; private set; } = "Pick up";
+    
     [SerializeField] private float splatVelocity = 2;
+
+    private Rigidbody rb;
+    private Collider throwCollider;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        throwCollider = GetComponent<Collider>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -12,5 +24,22 @@ public class Throwable : StatusEffect
 
             TriggerStatusEffect(creature);
         }
+    }
+
+    public void Throw(Vector3 direction, float force)
+    {
+        throwCollider.enabled = true;
+        throwCollider.isTrigger = false;
+        transform.SetParent(null);
+        rb.useGravity = true;
+        rb.AddForce(direction * force);
+    }
+
+    public void Interact()
+    {
+        rb.velocity = Vector3.zero;
+        throwCollider.enabled = false;
+        rb.useGravity = false;
+        GetComponent<Food>().StopAllCoroutines();
     }
 }
