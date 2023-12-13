c2d179b3a44f2b55025d59df4f4fcedfba6770b2
Justin <justinstolk97@gmail.com>
2023-12-13 13:15:14 +0100

    Implemented Sequence Puzzle

---
+++ Assets/Scripts/Interactions/SequencePuzzleElement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequencePuzzleElement : MonoBehaviour, IInteractable
+{
+    public bool Subscribed { get; private set; }
+    [field: SerializeField] public string InteractionPrompt { get; private set; } = "Interact";
+
+    [SerializeField] private Material pingMaterial;
+    [SerializeField] private Material completionMaterial;
+    [SerializeField] private Material wrongMaterial;
+
+    private MeshRenderer meshRenderer;
+    private Material defaultMaterial;
+    private SequencePuzzle sequencer;
+
+    private bool internalClear;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        defaultMaterial = meshRenderer.material;
+    }
+
+    public void Interact() => PingSequencer();
+
+    public void SubscribeElement(SequencePuzzle sequence)
+    {
+        sequencer = sequence;
+        Subscribed = true;
+    }
+
+    public void OnPuzzleFailed()
+    {
+        StopCoroutine(nameof(MaterialShift));
+        StartCoroutine(MaterialShift(wrongMaterial));
+    }
+    
+    public void OnPuzzleClear()
+    {
+        Debug.Log("Cleared");
+        StopCoroutine(nameof(MaterialShift));
+        internalClear = true;
+        meshRenderer.material = completionMaterial;
+    }
+
+    private void PingSequencer()
+    {
+        if (sequencer.Cleared)
+            return;
+
+        StopCoroutine(nameof(MaterialShift));
+        StartCoroutine(MaterialShift(pingMaterial));
+        sequencer.Ping(this);
+    }
+
+    private IEnumerator MaterialShift(Material targetMaterial)
+    {
+        float timer = 0f;
+        meshRenderer.material = targetMaterial;
+
+        while(timer < sequencer.MaterialShiftTimer)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        if (!internalClear)
+        {
+            meshRenderer.material = defaultMaterial;
+        }
+    }
+
+}
