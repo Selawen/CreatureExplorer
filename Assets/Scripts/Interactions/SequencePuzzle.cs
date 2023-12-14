a4d079ae74b9553e15d9472207f9a04e31e52971
SyllverS <ajaniek@live.nl>
2023-12-14 12:10:13 +0100

    Squashed commit of the following:
    commit 8e32873967eba7b81223ac48f1693844f29e9f56
    Author: Justin <justinstolk97@gmail.com>
    Date:   Wed Dec 13 15:08:17 2023 +0100
    
        Added clear event
    
    commit c2d179b3a44f2b55025d59df4f4fcedfba6770b2
    Author: Justin <justinstolk97@gmail.com>
    Date:   Wed Dec 13 13:15:14 2023 +0100
    
        Implemented Sequence Puzzle

---
+++ Assets/Scripts/Interactions/SequencePuzzle.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SequencePuzzle : MonoBehaviour
+{
+    public float MaterialShiftTimer { get { return materialShiftTime; } }
+    public bool Cleared { get; private set; }
+
+    [SerializeField] private SequencePuzzleElement[] puzzleElements;
+    [SerializeField] private bool canPingMultipleTimes;
+    [SerializeField] private float materialShiftTime  = 2f;
+    [SerializeField] private UnityEvent onPuzzleCleared;
+
+    private int sequenceCount;
+    private Queue<SequencePuzzleElement> pingOrder;
+
+    private void Awake()
+    {
+        pingOrder = new Queue<SequencePuzzleElement>();
+        foreach(SequencePuzzleElement element in puzzleElements)
+        {
+            if (!element.Subscribed)
+            {
+                element.SubscribeElement(this);
+            }
+        }
+    }
+
+    public void Ping(SequencePuzzleElement element)
+    {
+        if (pingOrder.Contains(element) && !canPingMultipleTimes || Cleared) 
+            return;
+
+        pingOrder.Enqueue(element);
+        sequenceCount++;
+        if(sequenceCount == puzzleElements.Length)
+        {
+            Evaluate();
+        }
+    }
+
+    private void Evaluate()
+    {
+        sequenceCount = 0;
+        for (int i = 0; i < puzzleElements.Length; i++)
+        {
+            if(puzzleElements[i] != pingOrder.Dequeue())
+            {
+                // This isn't the right order, puzzle failed
+                foreach(SequencePuzzleElement element in puzzleElements)
+                {
+                    element.OnPuzzleFailed();
+                }
+                pingOrder.Clear();
+                return;
+            }
+        }
+        foreach (SequencePuzzleElement element in puzzleElements)
+        {
+            element.OnPuzzleClear();
+        }
+        onPuzzleCleared?.Invoke();
+        Cleared = true;
+    }
+
+    
+}
